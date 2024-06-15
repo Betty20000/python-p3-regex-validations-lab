1fbf7f385906fc4c1c71ffa4e51fdbfdece87dcf
Beatrice Gachigi <beatricegachigi@gmail.com>
2024-06-15 14:00:53 +0300

    test

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"John Cena"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"555-555-5555"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"john.cena1234@wwe.com"
 email_regex = re.compile(email_address)
